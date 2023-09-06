e0589992a29b6e9a0b19b597cf2961b26f16d99f
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-06 14:00:47 +0700

    Write tests for 'main' URL handling

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpResponse
 
-# Create your tests here.
+class MainTest(TestCase):
+
+    def test_main_url_is_exist(self):
+        response: HttpResponse = Client().get('/main/')
+        self.assertEquals(response.status_code, 200)
+    
+    def test_main_using_correct_template(self):
+        response: HttpResponse = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
