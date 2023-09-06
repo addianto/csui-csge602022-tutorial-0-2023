8e4ed17780bce9f9918b97d03092fcbafed96bd3
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-06 13:59:38 +0700

    Handle HTTP request to '/main/' URL

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def show_main(request: HttpRequest) -> HttpResponse:
+    context: dict = {
+        'name': 'The One and Only: Rickey Astley',
+        'class': 'PBP Int.',
+    }
+
+    return render(request, 'main.html', context)
