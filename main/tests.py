176d9d49c1e72a1c5fd9a6d5731bcc23b03a6b52
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-13 16:16:15 +0700

    Fix deprecated assertion method

--- main/tests.py
+++ main/tests.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 class MainTest(TestCase):
     def test_main_url_is_exist(self):
         response: HttpResponse = Client().get("/main/")
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_main_using_correct_template(self):
         response: HttpResponse = Client().get("/main/")
