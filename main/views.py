3540c93cebb2f0f697c1ab4c95289044810256fa
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-13 17:31:10 +0700

    green: Implement create & list product page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.urls import reverse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from main.forms import ProductForm
+from main.models import Product
 
 
 def show_main(request: HttpRequest) -> HttpResponse:
+    products = Product.objects.all()
+
     context: dict = {
         "name": "The One and Only: Rickey Astley",
         "class": "PBP Int.",
+        "products": products,
     }
 
     return render(request, "main.html", context)
+
+
+def create_product(request: HttpRequest) -> HttpResponse:
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse("main:show_main"))
+
+    context: dict = {
+        "form": form,
+    }
+
+    return render(request, "create_product.html", context)
