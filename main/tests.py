7c7e239b9b8643d9b99f2763b7416b3aa68acbfc
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-13 17:30:08 +0700

    red: Implement create & list product page

--- main/tests.py
+++ main/tests.py
@@ -4,10 +4,29 @@ from django.http import HttpResponse
 
 
 class MainAppTest(TestCase):
-    def test_main_url_is_exist(self):
+    def test_show_main_returns_ok(self):
         response: HttpResponse = Client().get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_main_using_correct_template(self):
+    def test_show_main_uses_correct_template(self):
         response: HttpResponse = Client().get("/")
         self.assertTemplateUsed(response, "main.html")
+
+    def test_create_product_returns_ok(self):
+        response: HttpResponse = Client().get("/create-product")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_create_product_uses_correct_template(self):
+        response: HttpResponse = Client().get("/create-product")
+        self.assertTemplateUsed(response, "create_product.html")
+
+    def test_create_product_redirects_when_success(self):
+        response: HttpResponse = Client().post(
+            "/create-product",
+            data={
+                "name": "Product",
+                "price": 42,
+                "description": "A test product",
+            },
+        )
+        self.assertRedirects(response, "/", response.status_code, HTTPStatus.OK)
