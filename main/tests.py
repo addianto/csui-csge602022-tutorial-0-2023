09d2da28c7f93595af68da1a577ba12259ce08a6
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-13 16:23:36 +0700

    red: Change main app path to root path

--- main/tests.py
+++ main/tests.py
@@ -1,12 +1,13 @@
+from http import HTTPStatus
 from django.test import TestCase, Client
 from django.http import HttpResponse
 
 
-class MainTest(TestCase):
+class MainAppTest(TestCase):
     def test_main_url_is_exist(self):
-        response: HttpResponse = Client().get("/main/")
-        self.assertEqual(response.status_code, 200)
+        response: HttpResponse = Client().get("/")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_main_using_correct_template(self):
-        response: HttpResponse = Client().get("/main/")
+        response: HttpResponse = Client().get("/")
         self.assertTemplateUsed(response, "main.html")
