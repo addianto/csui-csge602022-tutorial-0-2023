fc6b75a9a72cfdab8ab3c74d4abc9e337c453e77
Daya Adianto <dayaadianto@cs.ui.ac.id>
2023-09-22 10:30:51 +0700

    Implement basic authorization on main page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
@@ -11,6 +12,7 @@ from main.forms import ProductForm
 from main.models import Product
 
 
+@login_required(login_url="/login")
 def show_main(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all()
 
